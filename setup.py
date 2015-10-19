21903d904f5c39434ff5353a50472e69761d4297
Matthew Corner <mcorner@signiant.com>
2015-10-18 21:50:06 -0400

    setup: Changed directory structure to support distutils, and added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Maestro Framework',
+      version='0.1a',
+      description='Maestro ',
+      author='Matthew Corner / Signiant Inc.',
+      author_email='mcorner@signiant.com',
+      url='https://www.signiant.com',
+      packages=['maestro'],
+     )
