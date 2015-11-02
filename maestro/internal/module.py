e1f143850a30a28a78eca213801460f12d2ff30a
Matthew Corner <mcorner@signiant.com>
2015-11-02 17:01:13 -0500

    modules: Added some missing comments.

--- maestro/internal/module.py
+++ maestro/internal/module.py
@@ -34,10 +34,13 @@ class Module(object):
         self.run(kwargs=kwargs)
 
 def __pickle_method__(m):
-        if m.im_self is None:
-            return getattr, (m.im_class, m.im_func.func_name)
-        else:
-            return getattr, (m.im_self, m.im_func.func_name)
+    """
+    Pickling override for using pickle with instance methods.
+    """
+    if m.im_self is None:
+        return getattr, (m.im_class, m.im_func.func_name)
+    else:
+        return getattr, (m.im_self, m.im_func.func_name)
 
 copy_reg.pickle(types.MethodType, __pickle_method__)
 
@@ -53,30 +56,48 @@ class AsyncModule(Module):
     __logger__ = None
 
     def __init__(self,ioc):
+        """
+        Sets the current status and calls the superclass' init
+        """
         super(AsyncModule,self).__init__(ioc)
         self.status = NOT_STARTED
 
     def start(self,kwargs={}):
+        """
+        The main method to start a module. In Async, it will return immediately with the result from apply_async. 
+        """
         pool = Pool(processes=1)
 	self.status = RUNNING
 	result = pool.apply_async(self.__setup__,[kwargs],callback=self.__finish_internal__)
         return result
 
     def __setup__(self,kwargs):
+        """
+        Internal setup method, this should not be overridden unless you know what you're doing. Calls the run method.
+        """
+        #Set up logger
         self.__logger__ = log_to_stderr()
         self.__logger__.setLevel(INFO)
-        self.__logger__.info("Beginning..." + str(kwargs))
         return self.run(kwargs=kwargs)
 
     def __finish_internal__(self,callback_args):
+        """
+        Internal finish method. This should not be overridden unless you know what you're doing. Calls the finish method.
+        """
         self.status = DONE
         self.finish(callback_args)
 
     def finish(self, results):
+        """
+        Finish method, overridable but otherwise sticks the results into a variable, or sets the exception.
+        """
         if isinstance(results,Exception):
             self.exception = results
         else:
             self.result = results
 
     def log(self, message, level=INFO):
+        """
+        Log method, currently set to only print to the console. Thread-safe.
+        """
         self.__logger__.info(message)
