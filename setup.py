b9f46baf07c310221f11904afb972f3ee561b3c4
Mike Perttula <mperttula@signiant.com>
2024-05-13 09:26:14 -0400

    Decrease boto3 requirements for older oses

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 from distutils.core import setup
 
 setup(name='MaestroOps',
-      version='0.9.0',
+      version='0.9.1',
       description='Python Automation Framework for Development Operations Teams',
       author='Signiant DevOps',
       author_email='devops@signiant.com',
       url='https://www.signiant.com',
       packages=find_packages(),
       license='MIT',
-      install_requires=['boto3>=1.34.0,<2']
+      install_requires=['boto3>=1.33.1,<1.34.0']
      )
